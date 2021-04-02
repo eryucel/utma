e538a78bc1bf0f5c7cb8cf06795f38b9ed77bc7b
Hakan Eryücel <33849527+Hakan-er@users.noreply.github.com>
2021-04-03 02:13:50 +0300

    Add has_permission control

--- src/api/utma/datasets/permissions.py
+++ src/api/utma/datasets/permissions.py
@@ -6,3 +6,6 @@ class IsOwnerOrAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (obj.user == request.user) or request.user.is_superuser
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
