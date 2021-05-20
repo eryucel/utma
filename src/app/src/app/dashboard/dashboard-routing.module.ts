00f52fa99f95b15bf775ff28f4de1b32d6b34729
Hakan Eryücel <33849527+Hakan-er@users.noreply.github.com>
2021-05-21 01:11:54 +0300

    Added data-rows routing.

--- src/app/src/app/dashboard/dashboard-routing.module.ts
+++ src/app/src/app/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,7 @@ import {DashboardComponent} from "./dashboard.component";
 import {DatasetComponent, AddDatasetComponent, EditDatasetComponent} from "./components";
 import {NumberAttributesStepComponent} from "./components/number-attributes-step/number-attributes-step.component";
 import {CategoricalAttributesStepComponent} from "./components/categorical-attributes-step/categorical-attributes-step.component";
+import {DataRowsStepComponent} from "./components/data-rows-step/data-rows-step.component";
 
 const routes: Routes = [
   {
@@ -14,9 +15,10 @@ const routes: Routes = [
       {path: 'add-dataset', component: AddDatasetComponent},
       {
         path: 'edit-dataset', component: EditDatasetComponent, children: [
-          {path: '', redirectTo: 'categorical-attributes', pathMatch: 'full'},
+          {path: '', redirectTo: 'number-attributes', pathMatch: 'full'},
           {path: 'number-attributes', component: NumberAttributesStepComponent},
           {path: 'categorical-attributes', component: CategoricalAttributesStepComponent},
+          {path: 'data-rows', component: DataRowsStepComponent},
         ]
       },
     ]
