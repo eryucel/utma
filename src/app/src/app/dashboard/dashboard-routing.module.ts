b5f3f55cd6887e7ef2d9213e0a7c1fca3ad15a6e
Hakan Eryücel <33849527+Hakan-er@users.noreply.github.com>
2021-05-24 09:06:49 +0300

    Update routing paths.

--- src/app/src/app/dashboard/dashboard-routing.module.ts
+++ src/app/src/app/dashboard/dashboard-routing.module.ts
@@ -5,6 +5,7 @@ import {DatasetComponent, AddDatasetComponent, EditDatasetComponent} from "./com
 import {NumberAttributesStepComponent} from "./components/number-attributes-step/number-attributes-step.component";
 import {CategoricalAttributesStepComponent} from "./components/categorical-attributes-step/categorical-attributes-step.component";
 import {DataRowsStepComponent} from "./components/data-rows-step/data-rows-step.component";
+import {ResultComponent} from "./components/result/result.component";
 
 const routes: Routes = [
   {
@@ -13,6 +14,7 @@ const routes: Routes = [
       {path: '', redirectTo: 'datasets', pathMatch: 'full'},
       {path: 'datasets', component: DatasetComponent},
       {path: 'add-dataset', component: AddDatasetComponent},
+      {path: 'result', component: ResultComponent},
       {
         path: 'edit-dataset', component: EditDatasetComponent, children: [
           {path: '', redirectTo: 'number-attributes', pathMatch: 'full'},
--- src/app/src/app/dashboard/dashboard.module.ts
+++ src/app/src/app/dashboard/dashboard.module.ts
@@ -1,17 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from './dashboard.component';
 import {DatasetComponent} from './components';
 import {AddDatasetComponent} from './components';
 import {SharedModule} from "../shared";
-import {MaterialModule} from "../shared/material.module";
 import {NgxDropzoneModule} from "ngx-dropzone";
-import { EditDatasetComponent } from './components/edit-dataset/edit-dataset.component';
-import { NumberAttributesStepComponent } from './components/number-attributes-step/number-attributes-step.component';
-import { CategoricalAttributesStepComponent } from './components/categorical-attributes-step/categorical-attributes-step.component';
-import { DataRowsStepComponent } from './components/data-rows-step/data-rows-step.component';
+import {EditDatasetComponent} from './components';
+import {NumberAttributesStepComponent} from './components/number-attributes-step/number-attributes-step.component';
+import {CategoricalAttributesStepComponent} from './components/categorical-attributes-step/categorical-attributes-step.component';
+import {DataRowsStepComponent} from './components/data-rows-step/data-rows-step.component';
+import {ResultComponent} from './components/result/result.component';
 import {ChartsModule} from "ng2-charts";
 
 
@@ -23,7 +22,8 @@ import {ChartsModule} from "ng2-charts";
     EditDatasetComponent,
     NumberAttributesStepComponent,
     CategoricalAttributesStepComponent,
-    DataRowsStepComponent
+    DataRowsStepComponent,
+    ResultComponent
   ],
   imports: [
     CommonModule,
